00d0119780f591718f1cbf6213fd5a9ccf51acd2
Akash P S <116643229+ItzmeAkash@users.noreply.github.com>
2023-09-27 18:08:13 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+from setuptools import find_packages, setup
+
+
+setup(
+    name = 'wasteDetection',
+    version = '0.0.0',
+    author = 'ItzmeAkash',
+    author_email = 'itzmeakashps@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
